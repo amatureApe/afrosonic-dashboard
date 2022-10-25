e77dabf5f8c4ec0fe4143480c72dff70b44e9f12
amatureApe <amatureApe@gmail.com>
2022-10-25 17:51:34 -0500

    implement genre carousel

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -2,7 +2,7 @@ import { VStack, HStack } from '@chakra-ui/react'
 
 import { Sidebar } from 'components/sidebar'
 import { SearchBar } from 'components/search-bar'
-
+import { GenresCarousel } from 'components/genres-carousel'
 
 export const Content = () => {
   return (
@@ -10,6 +10,7 @@ export const Content = () => {
       <Sidebar />
       <VStack px={12} pt={12} width="full" height="full" spacing={6} overflow="hidden">
         <SearchBar />
+        <GenresCarousel />
         <HStack width="full" alignItems="flex-start" overflow="hidden" flex={1}>
 
         </HStack>
--- src/components/genres-carousel/index.tsx
+++ src/components/genres-carousel/index.tsx
@@ -1,6 +1,11 @@
 import { HStack, VStack, Text, Box, LinkBox, LinkOverlay, useBreakpointValue } from '@chakra-ui/react'
 
+import { genres } from './genres-data'
+import { Image } from 'components/image'
+
 export const GenresCarousel = () => {
+  const imageSize = useBreakpointValue({ base: '80px', md: '96px', '2xl': '192px' }, '2xl')
+
   return (
     <HStack
       width="full"
@@ -9,7 +14,30 @@ export const GenresCarousel = () => {
       rounded="lg"
       flexShrink={0}
     >
-
+      {genres.map(({ name, image }, index) => (
+        <LinkBox as="article" pb={3} role="group" key={`${index}-${name}`}>
+          <VStack spacing={4}>
+            <Box rounded="lg" overflow="hidden" width={imageSize} height={imageSize}>
+              <LinkOverlay href="#">
+                <Image
+                  transition="ease-out"
+                  transitionProperty="all"
+                  transitionDuration="normal"
+                  _groupHover={{
+                    transform: 'scale(1.1, 1.1)'
+                  }}
+                  src={image}
+                  width={imageSize}
+                  height={imageSize}
+                  objectFit="cover"
+                  quality="100"
+                />
+              </LinkOverlay>
+            </Box>
+            <Text fontWeight="medium"></Text>
+          </VStack>
+        </LinkBox>
+      ))}
     </HStack>
   )
 }
