b84ad3c95cd0fdb33029257a22b4c313cf0970e4
amatureApe <amatureApe@gmail.com>
2022-10-25 17:33:25 -0500

    add types and images for carousel

---
+++ src/components/genres-carousel/index.tsx
@@ -0,0 +1,15 @@
+import { HStack, VStack, Text, Box, LinkBox, LinkOverlay, useBreakpointValue } from '@chakra-ui/react'
+
+export const GenresCarousel = () => {
+  return (
+    <HStack
+      width="full"
+      spacing={12}
+      overflowX="auto"
+      rounded="lg"
+      flexShrink={0}
+    >
+
+    </HStack>
+  )
+}
